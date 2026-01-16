98efa6c196a7a44d20a11c74803454d833320cf7
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-16 14:50:05 +0530

    convert product into classes

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -5,7 +5,7 @@ import { formatMoney } from './utils/money.js';
 let productsHTML = ''
 products.forEach((product) => {
   productsHTML += `
-         <div class="product-container">
+        <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
               src="${product.image}">
@@ -17,14 +17,14 @@ products.forEach((product) => {
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
+              src="${product.getStarsUrl()}">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${formatMoney(product.priceCents)}
+            ${product.getPrice()}
           </div>
 
           <div class="product-quantity-container">
@@ -49,14 +49,13 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}"}>
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
      `;
 
 });
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -33,7 +33,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${formatMoney(matchingProduct.priceCents)}
+                  $${matchingProduct.getPrice()}
                 </div>
                 <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                   <span>
