3c8a204875e9809355cb4ec80dac6f520d902009
pankaj rathod <pankajrathod272004@gmail.com>
2026-01-18 20:08:15 +0530

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,10 +1,51 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/backend-practice.js';
 
-loadProducts(() =>{
-   renderPaymentSummary();
-   renderOrderSummary();
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+
+  new Promise((resolve) => {
+      loadCart(() => {
+        resolve("value2");
+      });
+   }) 
+]).then((values) => {
+  console.log(values);
+  renderPaymentSummary();
+  renderOrderSummary();
 })
+
+
+// new Promise((resolve) =>{
+//      loadProducts(() =>{
+//         resolve("value1");
+//      });
+
+// }).then((value) => {
+//   console.log(value);
+//     return new Promise((resolve) => {
+//        loadCart(() => {
+//         resolve();
+//        });
+//     });
+
+// }).then(() => {
+//    renderPaymentSummary();
+//    renderOrderSummary();
+// })
+
+
+// loadProducts(() =>{
+//   loadCart(() => {
+//    renderPaymentSummary();
+//    renderOrderSummary();
+//   });
+// });
 
